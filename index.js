9e1195ae5829c23df0de85a028d04c953056c565
d-slezak <Slezak.Dan@outlook.com>
2022-06-28 19:40:24 -0400

    Add HTML CSS and JS for form to pop up

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 let library = [];
 
-function Book(title, author, series, pages, isRead){
+function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
-    this.series = series;
     this.pages = pages;
     this.isRead = isRead;
 }
 
+function addBook(book){
+    library.push(book)
+}
+
+function removeBook(book)
+{
+    library.pop(book);
+}
+
+const addBookBtn = document.getElementById('addBook');
+const bookGrid = document.getElementById('mainContent');
+const popUp = document.getElementById('popUp');
+const addBookForm = document.getElementById('addBookForm');
+
+const openPopUp = () => {
+    console.log("ey Yo")
+    popUp.classList.add('active');
+}
+
+addBookBtn.onclick = openPopUp;
