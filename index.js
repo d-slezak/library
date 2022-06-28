70eeef5fe3bac2015fd2e8f9f820cf6d08a06c30
d-slezak <Slezak.Dan@outlook.com>
2022-06-28 19:05:18 -0400

    Add Book Object

---
+++ index.js
@@ -0,0 +1,10 @@
+let library = [];
+
+function Book(title, author, series, pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.series = series;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
